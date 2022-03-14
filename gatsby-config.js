3e866229e089487568f37287a48ea9ea63e31bb6
Benjamin Lea <benlea123@gmail.com>
2022-03-14 12:57:25 +1100

    Add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,5 +11,20 @@ module.exports = {
     "gatsby-plugin-sitemap",
     "gatsby-plugin-vanilla-extract",
     "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        lang: "en-AU",
+        dir: "ltr",
+        name: "Frontend Mentor Challenges",
+        short_name: "FM",
+        description:
+          "An iOS engineer's attempt at learning CSS and Javascript.",
+        start_url: "/",
+        theme_color: "#f7c948",
+        background_color: "#f7f7f7",
+        icon: "src/images/favicon.png",
+      },
+    },
   ],
 };
